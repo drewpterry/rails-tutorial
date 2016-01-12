90997f1f18c920352a9ce5b4bdf49b9a470618c6
Drew Terry <drewpterry@yahoo.com>
2016-01-12 16:45:28 +0900

    refactored password reset user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,13 @@ class UsersController < ApplicationController
   end
 
   def index
-     @users = User.paginate(page: params[:page]) #params page is automatically generated
+    # @users = User.paginate(page: params[:page]) #params page is automatically generated
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    #writes debug information to terminal, allows access to rails console to debug
-    #debugger
+    redirect_to root_url and return unless @user.activated?
   end
   
   def create
