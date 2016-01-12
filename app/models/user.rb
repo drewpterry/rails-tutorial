337603887c9eeae4f4ee0e2c533506b0e1123044
Drew Terry <drewpterry@yahoo.com>
2016-01-12 16:11:04 +0900

    Add password resests and email config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token #visible to outside of object, can read and write
+  attr_accessor :remember_token, :activation_token , :reset_token#visible to outside of object, can read and write
   before_save :downcase_email
   #self.email.downcase is optional
   # before_save { email.downcase! } #mutates the actual string
@@ -52,6 +52,21 @@ class User < ActiveRecord::Base
     # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   # get 'sessions/new'
   get 'users/new' #this enables all the routes for user resource
 
@@ -17,6 +21,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # root 'application#hello'
   # controller name#function name
